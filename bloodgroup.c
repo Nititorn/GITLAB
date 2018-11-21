18808c74f7031e4a9f2e5e3bf71419f0b4e79684
nititorn <nititornsun@gmail.com>
2018-11-21 16:28:32 +0700

    modify on program1 if(a) and if(b)

--- bloodgroup.c
+++ bloodgroup.c
@@ -16,13 +16,11 @@ void main()
                             strcmp(G, "O") !=0 );
     if (strcmp(G,"A") ==0)
     {
-      printf("%s, A. Hey, you can give blood to: A, AB.\n", Name);
-      printf("  You can receive blood from: A, O.\n");
+      printf("HELLO you are A type\n");
     }
     else if (strcmp(G, "B") ==0)
     {
-      printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
-      printf("  You can receive blood from: B, O.\n");
+      printf("%s, B. Well, you can give blood to who you want to give\n", Name);
     }
     else if (strcmp(G, "AB") == 0)
     {
